64324bf27a8f3494bdf7e0b1867e46c11f985bc8
geena-keem <geena.gamm@gmail.com>
2022-05-01 20:45:07 +0900

    firebase install & settings

---
+++ src/service/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_APP_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
