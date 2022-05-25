994bc6f2e5ce78477b05bc50e744b1637aa40eb5
geena-keem <geena.gamm@gmail.com>
2022-05-26 00:30:09 +0900

    card & edit 컴포넌트 작업

--- src/component/maker/maker.jsx
+++ src/component/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import Footer from '../footer/footer';
@@ -8,6 +8,42 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'geena1',
+            company: 'geena',
+            theme: 'dark',
+            title: 'front',
+            email: 'geena@gmail.com',
+            message: 'go for it',
+            fileName: 'geena',
+            fileURL: null,
+        },
+        {
+            id: '2',
+            name: 'geena2',
+            company: 'geena',
+            theme: 'light',
+            title: 'front',
+            email: 'geena@gmail.com',
+            message: 'go for it',
+            fileName: 'geena',
+            fileURL: 'geena.png',
+        },
+        {
+            id: '3',
+            name: 'geena3',
+            company: 'geena',
+            theme: 'colorful',
+            title: 'front',
+            email: 'geena@gmail.com',
+            message: 'go for it',
+            fileName: 'geena',
+            fileURL: null,
+        },
+    ]);
+
     const history = useHistory();
 
     const onLogout = () => {
@@ -26,8 +62,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer />
         </section>
